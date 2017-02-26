216ee77c3625b4d074e5e7703dc9973bdf75649d
Colin Maher <colinmaher77@yahoo.com>
2017-02-26 12:17:11 +0000

    added server side code foor creating subscriptions

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,56 +1,70 @@
 /* global $, Stripe */
+//Document ready.
 $(document).on('turbolinks:load', function(){
-    var theForm = $('#pro_form');
-    var submitBtn = $('form-submit-btn');
-    //Set Stripe public key;
-    Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content'));
-    //When user clicks form submit btn
-    submitBtn.click(function(event){
-        //prevent default submission of fields
-            event.preventDefault();
-            submitBtn.val("Processing").prop('disabled', true);
-            //collect the card fields
-            var ccNum = $('#card_number').val(),
-                cvcNum = $('#card_code').val(),
-                expMonth = $('#card_month').val(),
-                expYear = $('#card_year').val();
-            //Send the card info to stripe
-            
-        
-        //Stripe will return a card token
-        //Inject card token as a hidden field to form
-        //Submit form to our Rails app
-        var error = false;
-        //Validate CVC number
-        if (!Stripe.card.validateExpiry(cvcNum)) {
-            error = true;
-            alert('The expiration date appears to be invalid');
-        }
-        
-        if (!Stripe.card.validateExpiry(expMonth, expYear)) {
-            error = true;
-            alert('The expiration date appears to be invalid');
-        }
-        
-        if (error) {
-            submitBtn.prop('disabled', false).val("Sign Up");
-        } else {
-            //Send the are card info to Stripe
-            Stripe.createToken({
-               number: ccNum,
-               cvc: cvcNum,
-               exp_month: expMonth,
-               exp_year: expYear
-            }, stripeResponseHandler);
-        }
-        return false;
-    });
-    function stripeResponseHandler(status, response) {
-        //Get the token from the response.
-        var token = response.id;
-        //Inject the card token in a hidden field.
-        theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
-        //Submit form to our Rails app.
-        theForm.get(0).submit();
+  var theForm = $('#pro_form');
+  var submitBtn = $('#form-signup-btn');
+  //Set Stripe public key.
+  Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
+  //When user clicks form submit btn,
+  submitBtn.click(function(event){
+    //prevent default submission behavior.
+    event.preventDefault();
+    submitBtn.val("Processing").prop('disabled', true);
+    //Collect the credit card fields.
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+    //Use Stripe JS library to check for card errors.
+    var error = false;
+    //Validate card number.
+    if(!Stripe.card.validateCardNumber(ccNum)) {
+      error = true;
+      alert('The credit card number appears to be invalid');
     }
+    //Validate CVC number.
+    if(!Stripe.card.validateCVC(cvcNum)) {
+      error = true;
+      alert('The CVC number appears to be invalid');
+    }
+    //Validate expiration date.
+    if(!Stripe.card.validateExpiry(expMonth, expYear)) {
+      error = true;
+      alert('The expiration date appears to be invalid');
+    }
+    if (error) {
+      //If there are card errors, don't send to Stripe.
+      submitBtn.prop('disabled', false).val("Sign Up");
+    } else {
+      //Send the card info to Stripe.
+      Stripe.card.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    return false;
+  });
+function stripeResponseHandler(status, response) {
+
+  if (response.error) { // Problem!
+
+    // Show the errors on the form
+    theForm.find('.payment-errors').text(response.error.message);
+    theForm.find('button').prop('disabled', false); // Re-enable submission
+
+  } else { // Token was created!
+
+    // Get the token ID:
+    var token = response.id;
+
+    // Insert the token into the form so it gets submitted to the server:
+    theForm.append($('<input type="hidden" name="user[stripe_card_token]" />').val(token));
+
+    // Submit the form:
+    theForm.get(0).submit();
+
+  }
+}
 });
